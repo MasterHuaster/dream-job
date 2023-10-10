fccd9a29a038a99035b45035a037fbf2d168d308
master <master@huaster.com>
2023-10-11 02:09:07 +0500

    Fixed a number of problems with fixtures

--- tests/unit/fixtures/TaskFixture.php
+++ tests/unit/fixtures/TaskFixture.php
@@ -1,9 +1,10 @@
 <?php
-namespace tests\fixtures;
+namespace tests\unit\fixtures;
 
 use yii\test\ActiveFixture;
 
 class TaskFixture extends ActiveFixture
 {
+    public $dataFile = '@tests/_data/task.php';
     public $modelClass = 'app\models\Task';
 }
